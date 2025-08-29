2fa880444cda4ccc419baf753c5ca0762d29e743
Bhoumik <Bhoumikchopra12@gmail.com>
2025-08-29 11:17:54 +0530

    fault corrected

--- controller/test.js
+++ controller/test.js
@@ -16,7 +16,7 @@ const handleBfhlRequest = (req, res) => {
     const email = "bhoumikchopra2022@vitbhopal.ac.in"
     const response = {
       is_success: true,
-      user_id:`${name}+${getDdmmyyyyFromTimestamp()}`,
+      user_id:`${name}-${getDdmmyyyyFromTimestamp()}`,
       email,
       roll_number: '22BCE10108',
       odd_numbers: oddNumbers,
